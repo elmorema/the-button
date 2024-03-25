ed14b46e79239bca6bb4031c46cfced687094679
elmorema <markielmore@gmail.com>
2024-03-24 21:00:12 -0400

    attempt 3

--- index.php
+++ index.php
@@ -20,22 +20,11 @@
 	</h4> 
 
 	<?php
-	  set_include_path(get_include_path() . PATH_SEPARATOR . 'phpseclib');
-          include('Net/SSH2.php');
 	
 		if(isset($_POST['button1'])) { 
 			echo "This is Button1 that is selected";
-			$ssh = new Net_SSH2('www.domain.tld');
-			if (!$ssh->login('username', 'password')) {
-    				exit('Login Failed');
-			}
-
-			echo $ssh->exec('pwd');
-			echo $ssh->exec('ls -la');
-			echo "got here";
-
-
-
+			exec("ssh root@192.168.86.1 -p password", $output);
+			echo $output;
 		} 
 		if(isset($_POST['button2'])) { 
 			echo "This is Button2 that is selected"; 
